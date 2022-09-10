c89a1f14b3d7cdb4f13872e133984c9f71bb258d
turaa <ermiaskelbessa@gmail.com>
2022-09-09 23:12:55 -0700

    uglier code

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const winMatch = [
-    ['1','2','3'],
-    ['4','5','6'],
-    ['7','8','9'],
-    ['1','4','7'],
-    ['2','5','8'],
-    ['3','6','9'],
-    ['1','5','9'],
-    ['3','5','7'],
-]
-
-let XorO = 'X'
-let over = false
-let statusArea = document.getElementById('status')
-let X_WON = 'X Won, <a onclick="location.reload()">Retry</a>'
-let O_WON = 'O Won, <a onclick="location.reload()">Retry</a>'
-let NO_WINNER = 'Nobody Won, <a onclick="location.reload()">Retry</a>'
+let WIN_MATCH = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7'],], X_O = 'X', over = false, statusArea = document.getElementById('status'), X_WON = 'X Won, <a onclick="location.reload()">Retry</a>', O_WON = 'O Won, <a onclick="location.reload()">Retry</a>', NO_WINNER = 'Nobody Won, <a onclick="location.reload()">Retry</a>'
 
 function IT(id){
     return document.getElementById(id).textContent.toString()
@@ -29,10 +13,10 @@ function disableBoxes(){
 let draw = 0
 function itemClicked(num){
     draw++
-    document.getElementById(num).innerHTML = '<h1>' + XorO + '</h1>'
+    document.getElementById(num).innerHTML = '<h1>' + X_O + '</h1>'
     document.getElementById(num).style.pointerEvents = 'none'
-    XorO == 'X' ? XorO = 'O' : XorO = 'X'
-    winMatch.forEach((match)=>{
+    X_O == 'X' ? X_O = 'O' : X_O = 'X'
+    WIN_MATCH.forEach((match)=>{
         if(IT(match[0]) == 'X' && IT(match[1]) == 'X' && IT(match[2]) == 'X'){
             statusArea.innerHTML = X_WON
             disableBoxes()
@@ -42,7 +26,6 @@ function itemClicked(num){
             disableBoxes()
             over = true
         }else{
-            console.log(draw)
             if(draw == 9 && !over){
                 statusArea.innerHTML = NO_WINNER
             }
