d6fb731d1f081501827ad24c34f82aef6cc37bee
turaa <ermiaskelbessa@gmail.com>
2022-09-09 20:59:57 -0700

    everybody loses

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ const winMatch = [
 ]
 
 let XorO = 'X'
-
-let statusArea = document.querySelector('.status')
+let over = false
+let statusArea = document.getElementById('status')
+let X_WON = 'X Won, <a onclick="location.reload()">Retry</a>'
+let O_WON = 'O Won, <a onclick="location.reload()">Retry</a>'
+let NO_WINNER = 'Nobody Won, <a onclick="location.reload()">Retry</a>'
 
 function IT(id){
     return document.getElementById(id).textContent.toString()
@@ -23,17 +26,26 @@ function disableBoxes(){
     }
 }
 
+let draw = 0
 function itemClicked(num){
+    draw++
     document.getElementById(num).innerHTML = '<h1>' + XorO + '</h1>'
     document.getElementById(num).style.pointerEvents = 'none'
     XorO == 'X' ? XorO = 'O' : XorO = 'X'
     winMatch.forEach((match)=>{
         if(IT(match[0]) == 'X' && IT(match[1]) == 'X' && IT(match[2]) == 'X'){
-            statusArea.innerHTML = 'X Won, <a onclick="location.reload()">Retry</a>'
+            statusArea.innerHTML = X_WON
             disableBoxes()
+            over = true
         }else if(IT(match[0]) == 'O' && IT(match[1]) == 'O' && IT(match[2]) == 'O'){
-            statusArea.innerHTML = 'O Won, <a onclick="location.reload()">Retry</a>'
+            statusArea.innerHTML = O_WON
             disableBoxes()
-        }else{}
+            over = true
+        }else{
+            console.log(draw)
+            if(draw == 9 && !over){
+                statusArea.innerHTML = NO_WINNER
+            }
+        }
     })
 }
